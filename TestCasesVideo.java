1e49a077b845fcf3db16e0097b932a0763966a5d
tcenov <tcenov@gmail.com>
2016-10-01 17:02:32 +0300

    update

---
+++ TestCasesVideo.java
@@ -0,0 +1,66 @@
+package android2.VideoEngager;
+
+import java.awt.AWTException;
+import java.io.IOException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+public class TestCasesVideo {
+
+	Firefox firefox = new Firefox();
+	static Android android = new Android();
+	
+	@Test(priority = 1)
+	public void androidLogin() throws InterruptedException, AWTException, IOException {
+		android.setUp();
+		android.login("tester2006@abv.bg", "Tarator1");
+		android.print("--------------------------------------------------------------------------------");
+	}
+
+	@Test(priority = 2)
+	public void firefoxJoin() throws InterruptedException, AWTException, IOException {
+		firefox.setUp();
+		firefox.join();
+		android.print("--------------------------------------------------------------------------------");
+	}
+	  
+	@Test(priority = 5)
+	public void prospectReceiveVideoCallEndFromAndroid() throws InterruptedException {
+		android.print("start new test ------------------------------------------------------------------");
+		android.startConversation();
+		android.startVideoCall();
+		android.pause(2);
+		firefox.answerVideoCall();
+		//ToDo verify video
+		android.pause(2);
+	//	firefox.muteMicrophone();
+		android.pause(2);
+		android.stopOrRejectVideoCall();
+		android.closeConversation();
+		android.print("Test case - prospect receive Video call");
+		android.print("--------------------------------------------------------------------------------");
+	}
+	 
+	@Test(priority = 7)
+	public void androidAgentReceiveVideoCall() throws InterruptedException {
+		android.print("start new test ------------------------------------------------------------------");
+		android.pause(2);
+		//firefox.callButtonFromHomeClick();
+		firefox.callButtonFromConversationClick();
+		android.pause(2);
+		android.answerVideoCall();
+		//ToDo verify video
+		android.pause(5);
+		firefox.muteMicrophone();
+		android.stopOrRejectVideoCall();
+		android.pause(2);
+		android.print("Test case - android agent receive video call,then android end video call ");
+		android.print("---------------------------------------------------------------------------");
+	}
+           
+	@AfterClass
+	void cleanUp() throws IOException {
+		android.cleanUpAndroid();
+		firefox.close();
+	}
+}
